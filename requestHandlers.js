4a0c8dd8c5ced5f9ffce3a74861f2f74283adc70
cmd-kvn <kvn.m.wong@gmail.com>
2017-02-12 01:38:48 -0800

    let upload page display received content, pg 37

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 
-function start(response) {
+function start(response, postData) {
     console.log('request handler "start" was called.');
 
     var body = '<html>' +
@@ -21,10 +21,10 @@ function start(response) {
     response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log('request handler "upload" was called.');
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('hello upload from reqHandlers.js');
+    response.write(`you've sent: ${postData}`);
     response.end();
 
 }
