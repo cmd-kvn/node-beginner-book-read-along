837777fee924f714e91dfd8267208010d8731693
cmd-kvn <kvn.m.wong@gmail.com>
2017-02-12 16:25:37 -0800

    updated router.js to page 45

--- router.js
+++ router.js
@@ -1,10 +1,9 @@
-function route(handle, pathname, response, postData) {
+/* pass request instead of postData */
+function route(handle, pathname, response, request) {
     console.log('About to route a request for ' + pathname);
 
-    // if (typeof handle[pathname] === 'function') handle[pathname]();
-    // else console.log(`no request handler found for ${pathname}`);
     if (typeof handle[pathname] === 'function') {
-        handle[pathname](response, postData);
+        handle[pathname](response, request);
     } else {
         console.log(`no request handler found for ${pathname}`);
         response.writeHead(404, { 'Content-Type': 'text/plain' });
